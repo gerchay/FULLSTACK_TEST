d86e08e994d6a96731e7d2ebb10ee7112f3a4e11
Gerardo cHAY <2995058550101@ingenieria.usac.edu.gt>
2021-04-06 23:48:10 -0600

    Función get con vanderas de texto reversible y booleano palindrome

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,6 +1,22 @@
 const { Router } = require('express')
 const router = Router()
 
-router.get('/', (req, res) => res.json({ text: 'Hello' }).status(200))
+router.get('/iecho', (req, res) => {
+  const { text } = req.query
+  if (text) {
+    // check it is palindrome
+    const isPalindrome = palindromeChecke(text)
+    // reverse string
+    const inverted = text.split('').reverse().join('')
+    res.json({ text: inverted, palindrome: isPalindrome }).status(200)
+  } else {
+    res.json({ error: 'no text' }).status(400)
+  }
+})
+
+const palindromeChecke = (str) => {
+  const strReversed = str.toLowerCase().split('').reverse().join('')
+  return strReversed === str
+}
 
 module.exports = router
